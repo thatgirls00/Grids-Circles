5cd62c6944a0672afb2ab552edba487c7e025d65
전현수 <wjsgustn201@gmail.com>
2025-09-23 11:37:15 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/org/example/caffe/CaffeApplication.java
@@ -0,0 +1,13 @@
+package org.example.caffe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaffeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaffeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/caffe/CaffeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.caffe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaffeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
