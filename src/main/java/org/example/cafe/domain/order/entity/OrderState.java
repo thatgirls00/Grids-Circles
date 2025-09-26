bc7e166df0cbb3a88ca1df4383acf019458bf092
yeongin <duddlsdlek610@gmail.com>
2025-09-26 12:18:27 +0900

    feat: 주문 상태 나타내는 enum 추가

--- src/main/java/org/example/cafe/domain/order/entity/OrderState.java
+++ src/main/java/org/example/cafe/domain/order/entity/OrderState.java
@@ -1,10 +1,8 @@
 package org.example.cafe.domain.order.entity;
 
 public enum OrderState {
-
     READY,       // 준비 중
-    CONFIRMED,   // 확정
-    COMPLETED,   // 완료
-    CANCELLED    // 취소
+    SHIPPING, // 배송 중
+    COMPLETED // 배송 완료
 }
 
