17d9f767d2325ac8bf3c97ee1c7f5c3bf0bb3ced
caketower <dlaqudtn1107@naver.com>
2025-09-26 17:53:37 +0900

    test : 임시저장용 커밋

--- src/main/java/org/example/cafe/domain/product/entity/Product.java
+++ src/main/java/org/example/cafe/domain/product/entity/Product.java
@@ -1,9 +1,6 @@
 package org.example.cafe.domain.product.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
