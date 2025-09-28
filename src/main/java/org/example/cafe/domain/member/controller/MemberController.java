19b640724d527ae3a72272a0cbaa78f0f0548853
yeongin <duddlsdlek610@gmail.com>
2025-09-28 21:13:02 +0900

    feat: 회원 이메일로 주문 내역 조회 기능 추가

--- src/main/java/org/example/cafe/domain/member/controller/MemberController.java
+++ src/main/java/org/example/cafe/domain/member/controller/MemberController.java
@@ -3,12 +3,12 @@ package org.example.cafe.domain.member.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.cafe.domain.member.entity.Member;
 import org.example.cafe.domain.member.service.MemberService;
+import org.example.cafe.domain.order.dto.reponse.OrderResponseDto;
 import org.example.cafe.global.rsData.RsData;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,5 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService memberService;
+
+    /**
+     * 회원 주문 내역 조회
+     */
+    @GetMapping("/{email}/orders")
+    public ResponseEntity<RsData<List<OrderResponseDto>>> getOrdersByEmail(
+            @PathVariable String email)
+    {
+        List<OrderResponseDto> orders = memberService.getOrdersByEmail(email);
+
+        return ResponseEntity.ok(new RsData<>("200", "주문 내역 조회 성공", orders));
+    }
 
 }
